48e8aab9db1c93f7b87481e07006dc5df76fcc83
Rob Kent <rob@Robs-MBP.dyn.lifesci.dundee.ac.uk>
2017-10-05 09:43:38 +0100

    Updated setup.py to create wheel for PyPi

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(name='QLed',
-      version='1.2',
-      description="An LED Widget for the PyQt4 Framework",
-      author="Rob Kent",
-      author_email="rob@gulon.co.uk",
-      url="http://http://code.google.com/p/gulon-soft/",
-      py_modules=['QLed'],
-      )
+    version='1.2.1',
+    description="An LED Widget for the PyQt4 Framework",
+    author="Rob Kent",
+    author_email="rob@gulon.co.uk",
+    url="https://github.com/jazzycamel/QLed",
+    py_modules=['QLed'],
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
