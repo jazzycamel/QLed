0cdfec6e796d75d98a3ff9480b397555808c9fea
Rob Kent <rob@Robs-MBP.dyn.lifesci.dundee.ac.uk>
2017-10-05 09:28:44 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='QLed',
+      version='1.2',
+      description="An LED Widget for the PyQt4 Framework",
+      author="Rob Kent",
+      author_email="rob@gulon.co.uk",
+      url="http://http://code.google.com/p/gulon-soft/",
+      py_modules=['QLed'],
+      )
